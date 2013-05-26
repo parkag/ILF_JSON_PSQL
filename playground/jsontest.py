9edd47293fde8557e5eb2681cda12c82b7b73d8e
parkag <grzegorz.parka@gmail.com>
2013-05-26 20:56:10 +0200

    A bit more of postgres code, plpythonu info

---
+++ playground/jsontest.py
@@ -0,0 +1,8 @@
+import json;
+
+String ='{
+ "betaArray" : {"value" : [0, 0, 0, 0, 1, 2], "pragma": "replace"}, 
+ "whatever" : {"value" : [3.1415], "pragma": "append"}.
+ "experimentState" : {"value": "Acquiring", "pragma": "transient"},
+ "acquiring" :  {"value": "Acquiring", "pragma": "transient"},
+}';
